214ada34e30fee9f2b3e6442bd880c6b9048313e
AgilePaul <nugapaul2@gmail.com>
2022-09-09 19:40:04 -0700

    Print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Programming is like building a multilingual puzzle
+ * main - prints Programming is like building a multilingual puzzle
  *
- * Return : Always 0 (Sucess)
+ * Return:Always 0 (Sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
