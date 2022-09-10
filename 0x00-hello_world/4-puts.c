4bb4d871e1b3f4037e53bff69bde7f5eb35c2e24
AgilePaul <nugapaul2@gmail.com>
2022-09-09 18:49:10 -0700

    Print statement with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Programming is like a multilingual puzzle
+ * main - Programming is like building a multilingual puzzle
  *
  * Return : Always 0 (Sucess)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
