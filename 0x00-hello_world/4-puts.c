2ee70e1956a3c1eac421fb97a8000cb807085d54
AgilePaul <nugapaul2@gmail.com>
2022-09-09 18:31:00 -0700

    Print statement using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Programming is like a multilingual puzzle
+ *
+ * Return : Always 0 (Sucess)
+int main (void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+return(0);
+}
